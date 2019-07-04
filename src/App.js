73bd94fdf8f7b88959b224f7fd46f1f8b2086d16
ShriLingam23 <trnshri23@gmail.com>
2019-07-04 18:19:45 +0530

    Basic crud implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Link,Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Create from './components/Create';
+import Edit from './components/Edit';
+import Show from './components/Show';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Route exact path='/' component={Home} />
+        <Route path='/edit/:id' component={Edit} />
+        <Route path='/create' component={Create} />
+        <Route path='/show/:id' component={Show} />
+      </div>
+    </BrowserRouter>
   );
 }
 
